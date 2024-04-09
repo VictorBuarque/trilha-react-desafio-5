db062f88f4e8a0f3e6458a2fd9de82e9cb6918cc
Victor Buarque <vcgabriel38@gmail.com>
2024-04-09 20:55:13 -0300

    feat: axios connection

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const api  = axios.create({
+    baseURL: 'https://xnfzogqzlnijualoalyg.supabase.co/rest/v1',
+    headers: {
+        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuZnpvZ3F6bG5panVhbG9hbHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxODg0MDMsImV4cCI6MjAyNzc2NDQwM30.8Q_JOWjIBC7fWef1tb9IFJhKREILV7xsBeNnsIg242Y",
+        authorization: "Bearer sbp_981a58931c0db6aaf9b9a6b3da05eb20390592e4"
+    }
+})

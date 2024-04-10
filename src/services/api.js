cac721be922e07851127b91a2e9ef84345f88cb3
Victor Buarque <vcgabriel38@gmail.com>
2024-04-09 23:05:57 -0300

    refactor: authorization key

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,6 @@ export const api  = axios.create({
     baseURL: 'https://xnfzogqzlnijualoalyg.supabase.co/rest/v1',
     headers: {
         apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuZnpvZ3F6bG5panVhbG9hbHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxODg0MDMsImV4cCI6MjAyNzc2NDQwM30.8Q_JOWjIBC7fWef1tb9IFJhKREILV7xsBeNnsIg242Y",
-        authorization: "Bearer sbp_981a58931c0db6aaf9b9a6b3da05eb20390592e4"
+        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhuZnpvZ3F6bG5panVhbG9hbHlnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTIxODg0MDMsImV4cCI6MjAyNzc2NDQwM30.8Q_JOWjIBC7fWef1tb9IFJhKREILV7xsBeNnsIg242Y"
     }
 })
